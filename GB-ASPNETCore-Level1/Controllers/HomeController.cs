6c45d926789b05675a1c5571e7d4033d8e82cb1a
Kuznetsov Viktor Vladimirovich <Arhio@yandex.ru>
2020-03-08 18:35:57 +0300

    ASP NET Core - Урок 2. Введение Razor и переписан ModelEmployee на Dictionary, что дало возможность EmployeesController переписать Details в одну строчку.

--- GB-ASPNETCore-Level1/Controllers/HomeController.cs
+++ GB-ASPNETCore-Level1/Controllers/HomeController.cs
@@ -1,36 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using WebStore.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult Index() => View();
 
-        public IActionResult Index()
-        {
-            //return Content("Home controllers - action Index");
-            return View();
-        }
+        public IActionResult SomeAction() => View();
 
-        public IActionResult SomeAction()
-        {
-            //return Content("Home controllers - action SomeAction");
-            return View();
-        }
+        public IActionResult Error404() => View();
 
-        public IActionResult Emploees()
-        {
-            return View(ModelEmployees._emploees);
-        }
+        public IActionResult Blog() => View();
 
-        public IActionResult Employee(int id)
-        {
-            var employee = ModelEmployees._emploees.FirstOrDefault(e => e.Id == id);
-            return employee is null? (IActionResult)NotFound() : View(employee);
-        }
+        public IActionResult BlogSingle() => View();
+
+        public IActionResult Cart() => View();
+
+        public IActionResult CheckOut() => View();
+
+        public IActionResult ContactUs() => View();
+
+        public IActionResult Login() => View();
+
+        public IActionResult Shop() => View();
+
+        public IActionResult ProductDetails() => View();
     }
 }
